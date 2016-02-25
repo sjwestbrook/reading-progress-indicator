a1853a02321d5eb3f4e54b214174c79aea1bdd3a
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-02-25 11:54:36 -0700

    Update progress.js

--- progress.js
+++ progress.js
@@ -1,15 +1,15 @@
 function updateArticle() {
   var scrollTop = $(window).scrollTop();
   
-  articles.each(function() {
+  articles.each(function() {    // articles - $('.cd-articles').children('article');
     var article = $(this);
-    var articleSidebarLink = articleSidebarLinks.eq(article.index()).children('a');
+    var articleSidebarLink = articleSidebarLinks.eq(article.index()).children('a');   // articleSidebarLinks - $('.cd-read-more').find('li');
     
-    if (articleTop > scrollTop) {
+    if (articleTop > scrollTop) {     // articleTop = $(this).offsett().top
       articleSidebarLink.removeClass('read reading');
     }
-    else if (scrollTop >= articleTop && articleTop + articleHeight > scrollTop) {
-      var dashoffsetValue = svgCircleLength * (1 - (scrollTop - articleTop) / articleHeight);
+    else if (scrollTop >= articleTop && articleTop + articleHeight > scrollTop) {  // articleHeight = $(this).outerHeight()
+      var dashoffsetValue = svgCircleLength * (1 - (scrollTop - articleTop) / articleHeight);     // svgCircleLength - 100
       articleSidebarLink.addClass('reading').removeClass('read').find('circle').attr({ 'stroke-dashoffsetValue'});
       changeUrl(articleSidebarLink.attr('href'));
     }
@@ -17,4 +17,19 @@ function updateArticle() {
       articleSidebarLink.removeClass('reading").addClass('read');
     }
   })
+}
+
+
+function updateSidebarPosition() {
+  var scrollTop = $(window).scrolltop();
+  
+  if (scrollTop < articlesWrapperTop) {     // $('.cd-articles').offset().top
+    aside.removeClass('fixed').attr('style', ''); // aside = $('.cd-read-more')
+  }
+  else if (scrollTop >= articlesWrapperTop && scrollTop < articlesWrapperTop + articlesWrapperHeight - windowHeight) {  // articlesWrapperHeight = $('.cd-articles')
+    aside.addClass('fixed').attr('style', '');
+  }
+  else {
+    if (aside.hasClass('fixed')) aside.removeClass('fixed').css('top', articlesWrapperHeight + articlePaddingTop - windowHeight + 'px');    // articlePaddingTop = Number($('.cd-articles').children('article').eq(1).css('padding-top').replace('px', ''))
+  }
 }
