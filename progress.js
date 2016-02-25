e4f6fbc49428becb3ac3e20d831ae6bda1b3b6d6
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-02-25 11:43:06 -0700

    Update progress.js

--- progress.js
+++ progress.js
@@ -1,1 +1,20 @@
-
+function updateArticle() {
+  var scrollTop = $(window).scrollTop();
+  
+  articles.each(function() {
+    var article = $(this);
+    var articleSidebarLink = articleSidebarLinks.eq(article.index()).children('a');
+    
+    if (articleTop > scrollTop) {
+      articleSidebarLink.removeClass('read reading');
+    }
+    else if (scrollTop >= articleTop && articleTop + articleHeight > scrollTop) {
+      var dashoffsetValue = svgCircleLength * (1 - (scrollTop - articleTop) / articleHeight);
+      articleSidebarLink.addClass('reading').removeClass('read').find('circle').attr({ 'stroke-dashoffsetValue'});
+      changeUrl(articleSidebarLink.attr('href'));
+    }
+    else {
+      articleSidebarLink.removeClass('reading").addClass('read');
+    }
+  })
+}
